aaed41e767824b652f6b7f9f836443d1858e0ae2
frankassafuah <frankassafuah@gmail.com>
2024-11-16 11:12:02 +0000

    create models, database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 # def home(request):
 #     return HttpResponse("<h1>Blog home</h1>")
 
-posts = [
-    {
-        "author": "CoreyMs",
-        "title": "Blog Post 1",
-        "content": "first post content",
-        "date_posted": "August 27, 2018",
-    },
-    {
-        "author": "Jane Doe",
-        "title": "Blog Post 2",
-        "content": "second post content",
-        "date_posted": "August 28, 2018",
-    },
-]
-
-
 def home(request):
-    context = {"posts": posts, }
+    context = {
+        "posts": Post.objects.all(),
+    }
     return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return render(request, "blog/about.html", {'title': 'About'})
+    return render(request, "blog/about.html", {"title": "About"})
