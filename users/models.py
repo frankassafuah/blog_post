7a7af5fbb78368b96b9bbb9d371be7bb2ceed0b5
frankassafuah <frankassafuah@gmail.com>
2024-11-23 21:04:55 +0000

    update profile info and profile photo

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -10,3 +11,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    def save(self): # this save method is a method that gets ran after the model has been saved. it exists in the parent model and we are creating our own
+        super().save() # ran save method of parent class
+    
+        #resizing image
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
